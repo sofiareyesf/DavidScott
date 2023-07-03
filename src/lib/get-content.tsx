34fbca6501617f4a27b88b4589b49886b849d7f6
owengretz <otgretz@gmail.com>
2023-07-02 23:35:45 -0400

    move content back to root

--- src/lib/get-content.tsx
+++ src/lib/get-content.tsx
@@ -1,10 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import { Type } from 'typescript';
 
 
-const contentFolder = path.join(process.cwd(), 'src', 'content');
+const contentFolder = path.join(process.cwd(), 'content');
 
 export default async function getContent(contentName: string) {
 
