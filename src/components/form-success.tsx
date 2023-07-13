f5a23fe15da9102e8876a63478716f079095cc62
owengretz <otgretz@gmail.com>
2023-07-12 21:45:22 -0400

    use shallow navigation to persist success message

--- src/components/form-success.tsx
+++ src/components/form-success.tsx
@@ -16,7 +16,7 @@ export default function FormSuccess() {
   useEffect(() => {
     if (searchParams.get('formsubmit')) {
       setFormSubmit(true);
-      router.push("/contact");
+      router.replace("/contact", {shallow: true});
     }
   }, []);
 
