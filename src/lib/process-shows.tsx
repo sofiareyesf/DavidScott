5cdfa71efcdfb6b34d9bbae08647b4752ea94045
owengretz <otgretz@gmail.com>
2023-08-12 19:58:32 -0400

    Update process-shows.tsx

--- src/lib/process-shows.tsx
+++ src/lib/process-shows.tsx
@@ -4,7 +4,7 @@ import { Show } from "./content-types";
 export default function processShows(shows: Show[]) {
   return shows.sort().map(show => {
     let showEndDateTime = new Date(show.date);
-    showEndDateTime.setHours(parseInt(show.end.slice(1, 3)) + 24, parseInt(show.end.slice(3, 5)));
+    showEndDateTime.setHours(parseInt(show.end.slice(1, 3)) + 4, parseInt(show.end.slice(3, 5))); // +4 for EST
     return {
       component: <ShowCard key={show.slug} show={show} />,
       endDateTimeValue: showEndDateTime.valueOf()
