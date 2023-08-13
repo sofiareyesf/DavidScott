8ba4d5410f84d5d693a1040f60c26f44abfa1b9d
owengretz <otgretz@gmail.com>
2023-08-12 22:56:44 -0400

    update opengraph image

--- src/components/open-graph-image.tsx
+++ src/components/open-graph-image.tsx
@@ -9,7 +9,7 @@ export default function OpenGraphImage() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center bg-bgcol text-textcol">
       <div className="w-[1200px] h-[600px] border border-accentcol relative">
-        <BackgroundImage src='/images/uploads/emmet-ray-2-cropped.jpeg' alt='Picture of me playing the saxophone' gradientDirClass='bg-gradient-to-t' middleColourPercentClass='via-25%' priority={true} sizes="100vw" />
+        <BackgroundImage src='/images/uploads/rex-centered.jpg' alt='Picture of me playing the saxophone' gradientDirClass='bg-gradient-to-t' middleColourPercentClass='via-25%' priority={true} sizes="100vw" />
 
         <div className="absolute bottom-16 w-full flex flex-col items-center text-center">
           {/* <h1 className="text-5xl md:text-7xl font-bold hidden md:block">Mattaus Gretzinger</h1>
